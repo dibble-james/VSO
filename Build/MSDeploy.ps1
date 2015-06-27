41c6c30af277e5282aac7239cbfa8d813124ba4e
James Dibble <james@jdibble.co.uk>
2015-06-28 00:50:19 +0100

    Adding basic MSDeploy script

---
+++ Build/MSDeploy.ps1
@@ -0,0 +1,63 @@
+[CmdletBinding()]
+param(
+	[Parameter(Mandatory)][string] $WebDeployPackage,
+	[Parameter(Mandatory)][string] $PackageDestinations,
+	[Parameter(Mandatory)][string] $VirtualDirectory,
+	[Parameter(Mandatory)][string] $WebDeployUser,
+	[Parameter(Mandatory)][string] $WebDeployPassword,
+    [string] $AgentType = "MSDepSvc",
+	[Switch] $AllowUntrusted,
+	[string] $WebDeployParameters
+)
+
+Write-Host -Foreground Green "Deploying $WebDeployPackage"
+
+Add-PSSnapin WDeploySnapin3.0
+
+$packageParameters = Get-WDParameters $WebDeployPackage
+
+if($WebDeployParameters)
+{
+	$deploymentValuesAsHashTable = ConvertFrom-StringData $WebDeployParameters
+	            
+	ForEach($deploymentValue in  $deploymentValuesAsHashTable.GetEnumerator())
+	{
+		if(!$packageParameters.ContainsKey($deploymentValue.Name))
+		{
+			Write-Host -Foreground Red "Package does not contain parameter $($deploymentValue.Name)"
+			Exit 1
+		}
+
+	    $packageParameters[$deploymentValue.Name] = $deploymentValue.Value
+	}	
+}
+
+$packageParameters['IIS Web Application Name'] = $VirtualDirectory
+
+$packageParametersVerbose = ($packageParameters | Out-String)
+
+Write-Verbose "Package Parameters $packageParametersVerbose" 
+
+ForEach($destination in $PackageDestinations.Split(','))
+{
+	try
+	{
+		Write-Host -Foreground Green "Deploying to $destination/$VirtualDirectory"
+
+		$settingsFilename = "$WebDeployPackage.$destination.publishsettings"
+
+		New-WDPublishSettings -AllowUntrusted -EncryptPassword -ComputerName $destination -UserId $WebDeployUser -Password $WebDeployPassword -FileName "$settingsFilename" -AgentType $AgentType
+
+		Write-Verbose "Using $settingsFilename"
+
+		Restore-WDPackage -Package $WebDeployPackage -Verbose -DestinationPublishSettings $settingsFilename -ErrorAction Stop -Parameters $packageParameters
+
+		Write-Host -Foreground Green "Sucessfully Deployed to $destination"
+	}
+	catch
+	{
+		Write-Host -Foreground Red "Failed to deploy to $destination`n$_"
+
+		Exit 1
+	}
+}
